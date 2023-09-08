19706ee0c1e3dc79434bea168760aeee8cd660be
user <user@LAPTOP-KAN0NU4I>
2023-09-08 12:25:44 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+
+        Dim iNumber As Integer
+
+
+
+        If IsNumeric(txtNum.Text) = True Then
+
+            iNumber = txtNum.Text
+            MsgBox(" You Entered Number is " & txtNum.Text)
+
+
+        Else
+            MsgBox("Try again, You didnt enter a number!")
+
+            Exit Sub
+        End If
+
+    End Sub
+End Class
